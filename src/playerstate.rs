027b37bf2867820aaf2a013a62d7fa278db1a7b4
troido <troido@protonmail.com>
2020-02-28 15:22:27 +0100

    make player movable

--- src/playerstate.rs
+++ src/playerstate.rs
@@ -6,7 +6,7 @@ use crate::{
 	componentwrapper::{ComponentWrapper, PreEntity},
 	PlayerId,
 	RoomId,
-	components::{Visible, Player, Inventory, Health, Fighter, Healing},
+	components::{Visible, Player, Inventory, Health, Fighter, Healing, Movable},
 	attack::Attack,
 	Result,
 	aerr,
@@ -117,7 +117,8 @@ impl PlayerState {
 			}),
 			ComponentWrapper::Health(Health{health: self.health, maxhealth: self.maximum_health}),
 			ComponentWrapper::Fighter(Fighter{attack: Attack::new(5), cooldown: 8}),
-			ComponentWrapper::Healing(Healing{delay: 50, health: 1, next_heal: None})
+			ComponentWrapper::Healing(Healing{delay: 50, health: 1, next_heal: None}),
+			ComponentWrapper::Movable(Movable{cooldown: 2})
 		]
 	}
 }
